a36567678b9d55aff3c222a34133519884f67fa5
scheidm <scheidm@wellcomposed.net>
2013-06-26 17:46:39 -0400

    new approach to forcing the 9.2 install, still not stable

--- dev-cookbooks/base/recipes/default.rb
+++ dev-cookbooks/base/recipes/default.rb
@@ -1,11 +1,14 @@
- apt_repository "postgresql" do
-   uri "http://ppa.launchpad.net/pitti/postgresql/ubuntu"
-   keyserver "keyserver.ubuntu.com"
-   key "8683D8A2"
-   components ["precise", "main"]
- end
+apt_repository "postgresql" do
+  uri "http://ppa.launchpad.net/pitti/postgresql/ubuntu"
+  keyserver "keyserver.ubuntu.com"
+  key "8683D8A2"
+  components ["precise", "main"]
+end
  
  package 'vim'
+directory '/etc/postgresql/9.2/main' do
+  recursive true
+end
+
 include_recipe "postgresql::server"
 include_recipe "postgresql::contrib"
-include_recipe 'postgis'
--- roles/setup.rb
+++ roles/setup.rb
@@ -5,6 +5,7 @@ run_list [
   'recipe[apt]',
   'recipe[git]',
   'recipe[openssl]',
+  'recipe[postgresql::apt_pgdg_postgresql]',
   'recipe[postgresql]',
   'recipe[base]'
 ]
@@ -17,7 +18,11 @@ default_attributes \
     pg_hba: [
       {type: 'local', db: 'all', user: 'all', addr: nil, method: 'trust'}
     ],
+    version: '9.2',
+    server: { packages: ["postgresql-9.2", "postgresql-server-dev-9.2", "postgresql-contrib-9.2"] },
+    enable_pgdg_apt: true,
     password: {
       postgres: '9678fd32f829d44e4eb45b0916b5adbc'
-    }
+    },
+  postgis:{}
   }
